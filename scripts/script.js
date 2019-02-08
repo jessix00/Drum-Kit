876acd7c9d47eb42c2da62d4700d2845c6073a01
jessix00 <jesscasillas24@gmail.com>
2019-02-08 11:58:47 -0700

    Playing css class is now added and removed depending on keys pressed.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 window.addEventListener('keydown', function(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`).classList.add('playing');
     if (!audio) return; //stop the function from running if there is no 
     audio.currentTime = 0; //Rewind to start of sound.This helps the sound play as soon as the key is pushed without delays
-    audio.play();
+    audio.play(); //plays audio
 });
+
+//Listens for key to be released and removes the 'playing' class
+window.addEventListener('keyup', function() {
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`).classList.remove('playing');
+})
