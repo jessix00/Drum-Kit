06f9126162d0a64ec6416ef17f34371c9cc7c774
jessix00 <jesscasillas24@gmail.com>
2019-02-08 12:29:44 -0700

    Restructured code. Now two functions/event listeners have been created.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
-window.addEventListener('keydown', function(event) {
+function playing() {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`).classList.add('playing');
-    if (!audio) return; //stop the function from running if there is no 
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!key) return;
+    key.classList.add('playing');
+    if (!audio) return; //stop the function from running if key doesnt match our audio selector 
     audio.currentTime = 0; //Rewind to start of sound.This helps the sound play as soon as the key is pushed without delays
     audio.play(); //plays audio
-});
-
+}
 //Listens for key to be released and removes the 'playing' class
-window.addEventListener('keyup', function() {
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`).classList.remove('playing');
-})
+function removeClass() {
+    const target = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!target) return;
+    target.classList.remove('playing'); //removes class of playing
+}
+
+//event listeners
+window.addEventListener('keydown', playing);
+window.addEventListener('keyup', removeClass);
