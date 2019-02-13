937ae5bc89d100f4303831a1d288af88ecfbc36b
jessix00 <jesscasillas24@gmail.com>
2019-02-13 16:18:31 -0700

    New function adds audio and divs programmatically. Working onClick event

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,33 @@
+const allKeys = document.querySelector(".keys");
+
+//template used to generate buttons on the document
+function init() {
+    //this variable holds the data for our keys
+    var dataKeys = [
+        { keyNumber: "65", keyName: "clap", key: "A" },
+        { keyNumber: "83", keyName: "hihat", key: "S" },
+        { keyNumber: "68", keyName: "kick", key: "D" },
+        { keyNumber: "70", keyName: "openhat", key: "F" },
+        { keyNumber: "71", keyName: "boom", key: "G" },
+        { keyNumber: "72", keyName: "ride", key: "H" },
+        { keyNumber: "74", keyName: "snare", key: "J" },
+        { keyNumber: "75", keyName: "tom", key: "K" },
+        { keyNumber: "76", keyName: "tink", key: "L" }
+    ];
+    //TODO: Update this loop to ES6
+    //this variable holds our divs once they're looped
+    let text = "";
+    for (var i = 0; i < dataKeys.length; i++) {
+        text += '<div data-key=' + '"' + dataKeys[i].keyNumber + '"' + 'class= "key"> <audio data-key= ' + '"' + dataKeys[i].keyNumber + '"' + 'src= sounds/' + dataKeys[i].keyName + '.wav></audio> <kbd>' + dataKeys[i].key + '</kbd> <span class= sound>' + dataKeys[i].keyName + ' </span></div>'
+    }
+    return allKeys.innerHTML = text;
+}
+init();
+
+
+
+
+//This will play our sound when a key is clicked.
 function playing(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
@@ -6,7 +36,10 @@ function playing(event) {
     if (!audio) return; //stop the function from running if key doesnt match our audio selector 
     audio.currentTime = 0; //Rewind to start of sound.This helps the sound play as soon as the key is pushed without delays
     audio.play(); //plays audio
+    console.log(audio);
+
 }
+
 //Listens for key to be released and removes the 'playing' class
 function removeClass() {
     const target = document.querySelector(`.key[data-key="${event.keyCode}"]`);
@@ -14,20 +47,21 @@ function removeClass() {
     target.classList.remove('playing'); //removes class of playing
 }
 
-//TODO: Separate event listener from function
-//when Key is clicked
-function clickKey() {
-    [...document.querySelectorAll('.key')].forEach(function(item) {
-        item.addEventListener('click', function() {
-            console.log(item);
-        });
-    });
-}
-
-clickKey();
-
-
-//event listeners
+// //event listeners
 window.addEventListener('keydown', playing);
 window.addEventListener('keyup', removeClass);
-//document.addEventListener('click', clickKey);
+
+
+
+//WORKING ON
+// //when Key is clicked
+// function clickKey() {
+//     const clickedAudio = Array.prototype.slice.apply(document.getElementsByTagName('audio'));
+//     clickedAudio.forEach((click) => {
+//         console.log(click);
+//     });
+
+// const clickedAudio = Array.prototype.slice.apply(document.getElementsByTagName('audio'));
+// clickedAudio.forEach((click) => {
+//         console.log(click)
+//window.addEventListener('click', clickKey);
